94467dc40e5fd0a7967938918f7ade89bcbaf264
AlexTrubkina <sashatru99@gmail.com>
2023-08-27 21:55:53 +0300

    feat: changed main page

---
+++ src/app/components/components/screens/homePage/HomePage.tsx
@@ -0,0 +1,12 @@
+import { FC } from 'react'
+import styles from './home.module.css'
+
+const HomePage : FC = () => {
+  return (
+      <main className={styles.main}>
+		<h1>Diagram</h1>
+    </main>
+  )
+}
+
+export default HomePage;
